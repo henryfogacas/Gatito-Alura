8662694039cef61f5888474c9fb07c26cd824416
henryfogacas <henryfogacas@gmail.com>
2024-02-01 09:37:56 -0300

    App Gatito

---
+++ src/telas/Servicos/Item/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { TouchableOpacity, Text, View } from "react-native";
+import Botao from "../../../componenentes/Botao";
+import CampoInteiro from "../../../componenentes/CampoInteiro";
+import estilos from "./estilos";
+
+export default function Item({ nome, preco, descricao }) {
+    const [quantidade, setQuantidade] = useState(1)
+    const [total, setTotal] = useState(preco)
+    const [expandir, setExpandir] = useState(false)
+
+    const atualizaQuantintadeTotal = (novaQuantidade) => {
+        setQuantidade(novaQuantidade)
+        calculaTotal(novaQuantidade)
+    }
+
+    const calculaTotal = (quantidade) => {
+        setTotal(quantidade * preco)
+    }
+
+    const inverteExpandir = () => {
+        setExpandir(!expandir)
+        atualizaQuantintadeTotal(1)
+    }
+
+    return <>
+        <TouchableOpacity style={estilos.informacao} onPress={inverteExpandir}>
+            <Text style={estilos.nome}>{nome}</Text>
+            <Text style={estilos.descricao}>{descricao}</Text>
+            <Text style={estilos.preco}>{Intl.NumberFormat('pt-BR', {
+                style: 'currency', currency: 'BRL'
+            }).format(preco)}</Text>
+        </TouchableOpacity>
+        {expandir &&
+            <View style={estilos.carrinho}>
+                <View>
+                    <View style={estilos.valor}>
+                        <Text style={estilos.descricao}>Quantidade:</Text>
+                        <CampoInteiro valor={quantidade} estilos={estilos.quantidade} acao={atualizaQuantintadeTotal} />
+                    </View>
+                    <View style={estilos.valor}>
+                        <Text style={estilos.descricao}>Total:</Text>
+                        <Text style={estilos.preco}>{Intl.NumberFormat('pt-BR', {
+                            style: 'currency', currency: 'BRL'
+                        }).format(total)}</Text>
+                    </View>
+                </View>
+                <Botao valor="Adicionar" acao={() => { }} />
+            </View>
+        }
+        <View style={estilos.divisor} />
+    </>
+}
